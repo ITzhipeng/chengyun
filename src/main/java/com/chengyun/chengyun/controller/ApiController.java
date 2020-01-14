cb4b47f2a6ee1ea29789a1759ec943ab867dd7e3
chengyun <18548156674@163.com>
2020-01-14 18:49:32 +0800

    首次提交1

--- src/main/java/com/chengyun/chengyun/controller/ApiController.java
+++ src/main/java/com/chengyun/chengyun/controller/ApiController.java
@@ -3,6 +3,6 @@ package com.chengyun.chengyun.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class ApiController {
+
 }
